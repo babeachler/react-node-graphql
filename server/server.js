c4306a1b38e8be7edcef288a9029d455468cb94f
Brent Beachler <brent@deepforktechnology.com>
2024-10-04 12:14:10 -0400

    adjusted used node modules, set up basic user crud ops

--- server/server.js
+++ server/server.js
@@ -1,28 +1,36 @@
-var express = require('express');
-var { createHandler } = require('graphql-http/lib/use/express');
-var { buildSchema } = require('graphql');
+// const express = require('express');
+// const { createHandler } = require('graphql-http/lib/use/express');
+// const { buildSchema } = require('graphql');
 
-var schema = buildSchema(`
-    type Query {
-    hello: String
-    }
-`);
+// const schema = buildSchema(`
+//     type Query {
+//         quoteOfTheDay: String
+//         random: Float!
+//         rollThreeDice: [Int]
+//     }
+// `);
 
-var root = {
-    hello() {
-        return 'Hello world!';
-    },
-};
+// const root = {
+//     quoteOfTheDay() {
+//         return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+//     },
+//     random() {
+//         return Math.random();
+//     },
+//     rollThreeDice() {
+//         return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
+//     },
+// };
 
-var app = express();
+// const app = express();
 
-app.all(
-    '/graphql',
-    createHandler({
-        schema: schema,
-        rootValue: root,
-    })
-);
+// app.all(
+//     '/graphql',
+//     createHandler({
+//         schema: schema,
+//         rootValue: root,
+//     })
+// );
 
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+// app.listen(4000);
+// console.log('Running a GraphQL API server at http://localhost:4000/graphql');
